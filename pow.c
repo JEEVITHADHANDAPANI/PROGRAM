a0e06c76917af7bf0ee692e93f7b2848209e7a94
JEEVITHADHANDAPANI <30828391+JEEVITHADHANDAPANI@users.noreply.github.com>
2017-09-05 15:46:17 +0530

    Create pow.c

---
+++ pow.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include<math.h>
+void main()
+{
+int a,b,c;
+printf("Enter the value for a and b");
+scanf("%d%d",&a,&b);
+c=pow(a,b);
+printf("the power is %d",c);
+getch();
+}
