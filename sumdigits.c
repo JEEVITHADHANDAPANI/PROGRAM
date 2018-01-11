9479f506ceaa1bebc35e27b329734f46663d0b03
JEEVITHADHANDAPANI <30828391+JEEVITHADHANDAPANI@users.noreply.github.com>
2018-01-11 16:13:14 +0530

    sumdigits.c

---
+++ sumdigits.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(void)
+{
+	int n,t,sum=0;
+  printf("Enter the number");
+  scanf("%d",&n);
+	while(n!=0)
+	{
+		t=n%10;
+		sum=sum+t;
+		n=n/10;
+	}
+	printf("sum is %d",sum);
+  return 0;
+}
