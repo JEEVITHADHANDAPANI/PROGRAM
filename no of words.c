00847b932244aeb2e226afc9360698d2a170784b
JEEVITHADHANDAPANI <30828391+JEEVITHADHANDAPANI@users.noreply.github.com>
2018-01-19 07:48:14 +0000

    no of words.c

---
+++ no of words.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<string.h>
+int main(void) {
+	char a[25];
+  int word=0,l;
+  printf("Enter the string");
+  scanf("%d",a);
+	l=strlen(a);
+	for(int i=0;i<=l;i++)
+	{
+		if(a[i]==' ')
+		++word;
+	}
+	printf("The no of word in a sentence is %d",word);
+	return 0;
+}
