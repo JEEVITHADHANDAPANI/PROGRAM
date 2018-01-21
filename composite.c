06953499b944b7365c946558178a35e63e2820ef
JEEVITHADHANDAPANI <30828391+JEEVITHADHANDAPANI@users.noreply.github.com>
2018-01-21 10:22:27 +0530

    composite.c

---
+++ composite.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+  int num,i,count=0;
+  printf("Enter the number");
+  scanf("%d",&num);
+  for(i=2;i<=num;i++)
+  {
+    if(num%i==0)
+    {
+      count++;
+    }
+  }
+  if(count>=3)
+  {
+    printf("%d is a composite number",num);
+  }
+    
+}
