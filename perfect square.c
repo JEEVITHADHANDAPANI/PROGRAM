39cd7bf865f3f5fa17bf8d70a717650ab1c78b45
JEEVITHADHANDAPANI <30828391+JEEVITHADHANDAPANI@users.noreply.github.com>
2018-01-21 11:40:45 +0530

    perfect square.c

---
+++ perfect square.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+  int num1,num2,i,count=0;
+  printf("Enter the number");
+  scanf("%d\t%d",&num1,&num2);
+  i=num1*num2;
+ if(sqrt(i)==num1&&sqrt(i)==num2)
+ {
+   printf("YES");
+ }
+ else
+ {
+   printf("NO");
+ }
+}
